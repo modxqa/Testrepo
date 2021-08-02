c0394a73f0d00247cad02d91516290a5a354a5e2
modxqa <88186286+modxqa@users.noreply.github.com>
2021-08-02 17:49:15 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,15 @@
+package com.utility.JiraAPI;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+public class App {
+	public static void main(String[] args) throws IOException, GeneralSecurityException {
+	
+			IssueCreation.initiate();
+			
+		
+	}
+
+	
+}
